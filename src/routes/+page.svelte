a245bd463f64592fef459c44a6997616fd2b4375
Anderson <MDAnderson@samaritan.org>
2025-05-06 14:08:02 -0400

    'na'

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,28 @@
 <script lang="ts">
-  import { Sun, Moon, LogOut } from "lucide-svelte";
+  import { Sun, Moon, LogOut, Github } from "lucide-svelte";
   import { toggleMode } from "mode-watcher";
   import Button from "$lib/components/ui/button/button.svelte";
   import { base } from "$app/paths";
   import { getFirebaseContext } from "$lib/firebase/firebase.svelte";
 
-
   const firebase = getFirebaseContext();
 </script>
 
 <div class="p-6 grid place-items-center">
-<div class="text-2xl text-center">
-  Welcome to  
-    <img 
-      src="{base}/favicon.png" 
-      alt="logo" 
-      class="inline w-24" 
-    />
-  stack
-  <Button onclick={toggleMode} variant="outline" size="icon" class="ml-2">
-    <Sun
-      class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-    />
-    <Moon
-      class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-    />
+  <div class="text-2xl text-center">
+    Welcome to
+    <img src="{base}/favicon.png" alt="logo" class="inline w-24" />
+    stack
+    <Button onclick={toggleMode} variant="outline" size="icon" class="ml-2">
+      <Sun class="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <Moon class="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+    </Button>
+  </div>
+  <Button href="https://github.com/forewit/fast-stack" class="mb-6"
+    ><Github /> View on GitHub</Button
+  >
+  <Button variant="destructive" onclick={firebase.logout} size="sm">
+    <LogOut class="h-4 w-4" />
+    Logout
   </Button>
-</div>
-<Button variant="destructive" onclick={firebase.logout} size="sm">
-  <LogOut class="h-4 w-4" />
-  Logout
-</Button>
-
 </div>
