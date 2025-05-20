09c69a55ccb62e94fc36d93ebd23adb5ab2fd572
forewit <marcda495@gmail.com>
2025-05-20 16:02:06 -0400

    Updates

--- app/immutable/chunks/BG0lDtIM.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_13t54c9)==null?void 0:s.base)??"/fast-stack";var t;const e=((t=globalThis.__sveltekit_13t54c9)==null?void 0:t.assets)??a;export{e as a,a as b};
--- app/immutable/entry/start.Dd2chgDs.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/BrD0LaTX.js";export{o as load_css,r as start};
