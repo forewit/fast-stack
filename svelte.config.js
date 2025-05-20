262f4d91b2407fb387909b91bf04c75caec093a1
forewit <marcda495@gmail.com>
2025-05-20 16:01:44 -0400

    'na'

--- svelte.config.js
+++ svelte.config.js
@@ -11,8 +11,14 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
-		appDir: 'app',
+		appDir: 'app', // gh-pages doesn't play with the default _app directory
+		adapter: adapter({
+			pages: 'build',      // The directory to write prerendered pages to
+			assets: 'build',     // The directory to write static assets to
+			fallback: 'index.html', // Or '200.html' or null if you want 404.html to handle all non-prerendered paths
+			precompress: false,  // Firebase can handle compression
+			strict: true         // Recommended
+		}),
 		paths: {
 			base: process.env.NODE_ENV === "production" ? "/fast-stack" : "",
 		}
